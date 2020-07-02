2201f7cf885b11014b07fdc454eb5b659e6ea6fc
Divyanshu Maithani <div.blackcat@gmail.com>
2020-07-02 21:14:36 +0530

    Add base example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [dbValue, saveToDb] = useState(""); // would be an API call normally
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Blog</h1>
+      <textarea
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        onchange
+        rows={5}
+        cols={50}
+      />
+      <section className="panels">
+        <div>
+          <h2>Editor (Client)</h2>
+          {value}
+        </div>
+        <div>
+          <h2>Saved (DB)</h2>
+          {dbValue}
+        </div>
+      </section>
+    </main>
   );
 }
 
