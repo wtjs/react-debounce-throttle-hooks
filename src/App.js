4ed83e14c3fb7115688855dd0a2bccf3e418ae0e
Akhil <akhil9tiet@gmail.com>
2021-04-12 04:17:41 -0700

    feat: componentize the debouncer

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
-import React, { useState, useCallback, useRef, useEffect } from "react";
-import debounce from "lodash.debounce";
+import React from 'react';
+import Debouncer from './Components/DebounceExample';
 
-import "./App.css";
-
-function useDebounce(callback, delay) {
-  const debouncedFn = useCallback(
-    debounce((...args) => callback(...args), delay),
-    [delay] // will recreate if delay changes
-  );
-  return debouncedFn;
-}
-
-function useDebounceAlt(callback, delay) {
-  const memoizedCallback = useCallback(callback, []);
-  const debouncedFn = useRef(debounce(memoizedCallback, delay));
-
-  useEffect(() => {
-    debouncedFn.current = debounce(memoizedCallback, delay);
-  }, [memoizedCallback, debouncedFn, delay]);
-
-  return debouncedFn.current;
-}
+import './App.css';
 
 function App() {
-  const [value, setValue] = useState("");
-  const [dbValue, saveToDb] = useState(""); // would be an API call normally
-
-  const debouncedSave = useDebounce((nextValue) => saveToDb(nextValue), 1000);
-
-  const handleChange = (event) => {
-    const { value: nextValue } = event.target;
-    setValue(nextValue);
-    debouncedSave(nextValue);
-  };
-
-  return (
-    <main>
-      <h1>Blog</h1>
-      <textarea value={value} onChange={handleChange} rows={5} cols={50} />
-      <section className="panels">
-        <div>
-          <h2>Editor (Client)</h2>
-          {value}
-        </div>
-        <div>
-          <h2>Saved (DB)</h2>
-          {dbValue}
-        </div>
-      </section>
-    </main>
-  );
+	return <Debouncer />;
 }
 
 export default App;
---
+++ src/Components/DebounceExample.js
@@ -0,0 +1,54 @@
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+import debounce from 'lodash.debounce';
+import './styles.css';
+
+function useDebounce(callback, delay) {
+	const debouncedFn = useCallback(
+		debounce((...args) => callback(...args), delay),
+		[delay] // will recreate if delay changes
+	);
+	return debouncedFn;
+}
+
+function useDebounceAlt(callback, delay) {
+	const memoizedCallback = useCallback(callback, []);
+	const debouncedFn = useRef(debounce(memoizedCallback, delay));
+
+	useEffect(() => {
+		debouncedFn.current = debounce(memoizedCallback, delay);
+	}, [memoizedCallback, debouncedFn, delay]);
+
+	return debouncedFn.current;
+}
+
+function Debouncer() {
+	const [value, setValue] = useState('');
+	const [dbValue, saveToDb] = useState(''); // would be an API call normally
+
+	const debouncedSave = useDebounce((nextValue) => saveToDb(nextValue), 1000);
+
+	const handleChange = (event) => {
+		const { value: nextValue } = event.target;
+		setValue(nextValue);
+		debouncedSave(nextValue);
+	};
+
+	return (
+		<main>
+			<h1>Blog</h1>
+			<textarea value={value} onChange={handleChange} rows={5} cols={50} />
+			<section className='panels'>
+				<div>
+					<h2>Editor (Client)</h2>
+					{value}
+				</div>
+				<div>
+					<h2>Saved (DB)</h2>
+					{dbValue}
+				</div>
+			</section>
+		</main>
+	);
+}
+
+export default Debouncer;
